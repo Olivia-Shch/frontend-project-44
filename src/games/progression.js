1cebe5b4dd36562a0c2d1efe4a493f2b0afae17e
vboxuser <vboxuser@Ubuntic.myguest.virtualbox.org>
2025-03-10 21:14:54 +1000

    >_<

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,14 +2,14 @@ import launchGame from '../index.js';
 import getRandomNumber from '../random.js';
 
 const getProgression = (start, step, length) => {
-  return Array.from({ length }, (_, i) => start + i * step);
+  Array.from({ length }, (_, i) => start + i * step)
 };
 
 const generateQuestionProgression = () => {
   const start = getRandomNumber(1, 50);
   const length = getRandomNumber(5, 10);
-  const step = getRandomNumber(-10, 10);
-  if (step === 0) step =1;
+  let step = getRandomNumber(-10, 10);
+  if (step === 0) step = 1;
   const skip = getRandomNumber(0, length - 1);
   const progression = getProgression(start, step, length);
   const correctAnswer = String(progression[skip]);
