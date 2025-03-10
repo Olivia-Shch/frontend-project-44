6c076c63bc51f15d594596328d5edb6eb67ad0fa
vboxuser <vboxuser@Ubuntic.myguest.virtualbox.org>
2025-03-10 12:46:18 +1000

    correction_2

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,19 +2,20 @@ import launchGame from '../index.js';
 import getRandomNumber from '../random.js';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num / 2; i += 1) {
-    if (num % i === 0) {
+  if (elem <= 1) return false;
+  for (let i = 2; i < Math.sqrt(elem); i += 1) {
+    if (elem % i === 0) {
       return false;
     }
   }
-
   return true;
 };
 
 const generateQuestionPrime = () => {
-  const number = getRandomNumber(200);
-  const correctAnswer = isPrime(number) ? 'yes' : 'no';
-  return [number, correctAnswer];
+  const randomNum = getRandomNumber(0, 100);
+  const correctAnswer = isPrime(randomNum) ? 'yes' : 'no';
+  const question = randomNum;
+  return { correctAnswer, question };
 };
 const rulesGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const startPrimeGame = () => launchGame(rulesGame, generateQuestionPrime);
