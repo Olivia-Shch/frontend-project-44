81eb34d89331a2bc10a4a9222485e95c35c52b99
vboxuser <vboxuser@Ubuntic.myguest.virtualbox.org>
2025-03-10 19:20:15 +1000

    lint_corr

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
-import globals from "globals";
-
+import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-];
+  { languageOptions: { globals: {...globals.browser, ...globals.node } } },
+]; 
